ec326ed30cfcaa1e55431ecaf53590cd6a6a85ee
Jonathan Hays <jonathanhays@Jonathans-MacBook-Pro-2.local>
2019-01-15 08:25:36 -0800

    Adding uuValidEmail to UUString

--- UUSwift/UUString.swift
+++ UUSwift/UUString.swift
@@ -154,6 +154,14 @@ public extension String
         
         return Data(base64Encoded: tmp, options: .ignoreUnknownCharacters)
     }
+	
+	// Checks if a given string conforms to a email@domain.suffix format
+	public func uuValidEmail() -> Bool {
+		let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+		
+		let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+		return emailTest.evaluate(with: self)
+	}
     
 }
 
