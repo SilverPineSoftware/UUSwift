5c67abd43581066ce606376d9c41403cf77c6edf
Ryan DeVore <ryan@silverpine.com>
2019-07-11 11:06:48 -1000

    Add method to check if string is a valid email  address

--- UUSwift/UUString.swift
+++ UUSwift/UUString.swift
@@ -155,5 +155,12 @@ public extension String
         return Data(base64Encoded: tmp, options: .ignoreUnknownCharacters)
     }
     
+    func uuIsValidEmail() -> Bool
+    {
+        let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let predicate = NSPredicate(format:"SELF MATCHES %@", regex)
+        return predicate.evaluate(with: self)
+    }
+    
 }
 
