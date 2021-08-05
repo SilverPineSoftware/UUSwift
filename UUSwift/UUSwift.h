0e36ce683d1540fbe9db3f49147de089e909e0f2
Jonathan Hays <jon@silverpinesoftware.com>
2021-08-04 22:22:02 -0700

    Fixing project configurations

---
+++ UUSwift/UUSwift.h
@@ -0,0 +1,18 @@
+//
+//  UUSwift.h
+//  UUSwift
+//
+//  Created by Jonathan Hays on 8/4/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UUSwift.
+FOUNDATION_EXPORT double UUSwiftVersionNumber;
+
+//! Project version string for UUSwift.
+FOUNDATION_EXPORT const unsigned char UUSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UUSwift/PublicHeader.h>
+
+
