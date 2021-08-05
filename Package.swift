e10facff898f524d57ac64f57797364161e4da6a
Jonathan Hays <jon@silverpinesoftware.com>
2021-08-04 22:12:46 -0700

    Updating source path

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let package = Package(
         .target(
             name: "UUSwift",
             dependencies: ["UUSwiftCore", "UUSwiftUX", "UUSwiftImage", "UUSwiftNetworking" ],
-            path: "UUSwift",
+            path: "",
             exclude: ["UUSwift-Info.plist"])
     ],
     swiftLanguageVersions: [
